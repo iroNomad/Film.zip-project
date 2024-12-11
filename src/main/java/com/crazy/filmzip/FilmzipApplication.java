ff26eb2edf87b9e9cb687443270745c1dfc185e2
nmskfkai <leejimin00@naver.com>
2024-12-11 10:59:54 +0900

    init

---
+++ src/main/java/com/crazy/filmzip/FilmzipApplication.java
@@ -0,0 +1,13 @@
+package com.crazy.filmzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmzipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crazy/filmzip/FilmzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crazy.filmzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
