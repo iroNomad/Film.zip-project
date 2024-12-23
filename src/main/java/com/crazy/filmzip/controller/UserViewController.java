3328fc19e563b0cca4e55cd2054f9ee6e75176b2
jinwon <jinwon0988@naver.com>
2024-12-23 14:57:53 +0900

    feat: 구글 로그인, 로그아웃 기능 추가

---
+++ src/main/java/com/crazy/filmzip/controller/UserViewController.java
@@ -0,0 +1,26 @@
+package com.crazy.filmzip.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "oauthLogin";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+
+    @GetMapping("/main")
+    public String getMain() {
+        return "main";
+    }
+}
--- src/main/java/com/crazy/filmzip/entity/User.java
+++ src/main/java/com/crazy/filmzip/entity/User.java
@@ -1,14 +1,17 @@
 package com.crazy.filmzip.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -16,29 +19,23 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String username;
+    private String password;
 
     @Column(nullable = false)
     private String nickname;
 
-    @Column(nullable = false)
-    private String password;
-
     private String profileImage;
-
-    private String socialProvider;
-
-    private String socialId;
 
     private String birthDate;
 
@@ -66,5 +63,55 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ToWatchMovie> toWatchMovies = new ArrayList<>();
+
+    // 생성자에 nickname 추가
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
 
 }
